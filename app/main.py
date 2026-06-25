48d72102e5a040933d08297724bf82b8fb71f5f0
Vaghani <nikunj.vaghani@siemens.com>
2026-06-25 15:19:12 +0200

    Add Playwright website crawler

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from app.services.crawler import get_page_title
 
 app = FastAPI(title="SEO Audit Agent")
+
 
 @app.get("/")
 def root():
     return {"message": "SEO Audit Agent is running!"}
+
+
+@app.get("/crawl")
+def crawl(url: str):
+    title = get_page_title(url)
+
+    return {
+        "url": url,
+        "title": title
+    }
